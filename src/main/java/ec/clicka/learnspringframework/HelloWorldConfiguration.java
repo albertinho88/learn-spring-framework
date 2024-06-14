034c5c8905247b2d9a681b85b4eb9aaf3af344ef
Jlarragamena <jlarragamena@trueblue.com>
2024-06-13 22:56:08 -0500

    First Java Spring bean and launching Java Spring Configuration

---
+++ src/main/java/ec/clicka/learnspringframework/HelloWorldConfiguration.java
@@ -0,0 +1,16 @@
+package ec.clicka.learnspringframework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+    @Bean
+    public String name() {
+        return "Ranga";
+    }
+
+
+
+}
